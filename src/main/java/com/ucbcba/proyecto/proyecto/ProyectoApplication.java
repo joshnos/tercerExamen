6d7e83809028c6875a6e300d55774f74f35cd6c4
Nicolaspdle96 <nicolaspdle@gmail.com>
2017-10-26 07:57:28 -0400

    prueba1

--- src/main/java/com/ucbcba/proyecto/proyecto/ProyectoApplication.java
+++ src/main/java/com/ucbcba/proyecto/proyecto/ProyectoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProyectoApplication {
-
+// Hola dudes
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
 	}
