d6399d24e27c67f0f588c43993695ef83b780864
Nicolaspdle96 <nicolaspdle@gmail.com>
2017-11-06 16:47:59 -0400

    fix

--- src/main/java/com/ucbcba/proyecto/proyecto/Repositories/OptionRepository.java
+++ src/main/java/com/ucbcba/proyecto/proyecto/Repositories/OptionRepository.java
@@ -2,9 +2,10 @@ package com.ucbcba.proyecto.proyecto.Repositories;
 
 import com.ucbcba.proyecto.proyecto.Entities.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
 
 @Transactional
-public interface OptionRepository extends JpaRepository<Option, Integer> {
+public interface OptionRepository extends CrudRepository<Option, Integer> {
 }
