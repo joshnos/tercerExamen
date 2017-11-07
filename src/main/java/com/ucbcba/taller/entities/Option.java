19928b9155bfaeaa533b79e64c0f9da5784b8b79
InaEte <inaeterovic@gmail.com>
2017-11-06 21:54:57 -0400

    add name field registration

---
+++ src/main/java/com/ucbcba/taller/entities/Option.java
@@ -0,0 +1,37 @@
+package com.ucbcba.taller.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "option")
+public class Option {
+    private long id;
+    private String name;
+    private int price;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/com/ucbcba/taller/entities/User.java
@@ -0,0 +1,72 @@
+package com.ucbcba.taller.entities;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by alvaro on 10/5/17.
+ */
+
+@Entity
+@Table(name = "user")
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private String passwordConfirm;
+    private String name;
+    private Set<Role> roles;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Transient
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    @ManyToMany
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
