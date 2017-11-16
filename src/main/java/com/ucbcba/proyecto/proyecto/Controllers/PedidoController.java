6ee8a1e3d2ce4e98a26d55203172fa9b4c237d67
Nicolaspdle96 <nicolaspdle@gmail.com>
2017-11-16 18:58:17 -0400

    fix

--- src/main/java/com/ucbcba/proyecto/proyecto/Controllers/PedidoController.java
+++ src/main/java/com/ucbcba/proyecto/proyecto/Controllers/PedidoController.java
@@ -3,6 +3,7 @@ package com.ucbcba.proyecto.proyecto.Controllers;
 import  com.ucbcba.proyecto.proyecto.Entities.Pedido;
 import com.ucbcba.proyecto.proyecto.Entities.User;
 import com.ucbcba.proyecto.proyecto.Services.EmpresaService;
+import com.ucbcba.proyecto.proyecto.Services.OptionService;
 import com.ucbcba.proyecto.proyecto.Services.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,16 +17,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class PedidoController {
 
+    private OptionService optionService;
     private EmpresaService empresaService;
     private PedidoService pedidoService;
     @Autowired
     public void setPedidoService(PedidoService pedidoService){this.pedidoService=pedidoService;}
     @Autowired
     public void setEmpresaService(EmpresaService empresaService){this.empresaService=empresaService;}
+    @Autowired
+    public void setOptionService(OptionService optionService){this.optionService = optionService;}
+
 
     @RequestMapping(value="/pedido/{id}", method = RequestMethod.GET)
     public String Pedir(@PathVariable Integer id, Model model){
         model.addAttribute("empresa",empresaService.getEmpresaById(id));
+        model.addAttribute("options",optionService.listAllOptions());
         model.addAttribute("pedido",new Pedido());
         return "pedidos";
     }
